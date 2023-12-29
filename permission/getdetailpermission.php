325898a45c2a88fd389cf967c3f2047546a34daa
kevinngabriell <kevingabriel786@gmail.com>
2023-12-29 20:34:54 +0700

    modified:   permission/getdetailpermission.php

--- permission/getdetailpermission.php
+++ permission/getdetailpermission.php
@@ -8,7 +8,7 @@ require_once('../../connection/connection.php');
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     $permission_id = $_GET['permission_id'];
 
-    $query = "SELECT A2.employee_name, A3.department_name, A4.permission_type_name, A2.employee_id, A5.position_name, A1.permission_date , A1.permission_reason, A1.permission_time, A6.permission_status_name, A6.id_permission_status, A2.employee_spv, (SELECT employee_name FROM employee WHERE id = A1.created_by) AS created_by, A1.created_dt AS created_dt, (SELECT employee_name FROM employee WHERE id = A1.created_by) AS created_by, DATE_FORMAT(A1.created_dt, '%e %M %Y %H:%i:%s') AS created_dt, (SELECT employee_name FROM employee WHERE id = A1.update_by) AS update_by, DATE_FORMAT(A1.update_dt, '%e %M %Y %H:%i:%s') AS update_dt, A1.start_cuti, A1.end_cuti, A1.cuti_phone, A1.pengganti_cuti
+    $query = "SELECT A2.employee_name, A3.department_name, A4.permission_type_name, A2.employee_id, A5.position_name, A1.permission_date , A1.permission_reason, A1.permission_time, A6.permission_status_name, A6.id_permission_status, A2.employee_spv, (SELECT employee_name FROM employee WHERE id = A1.created_by) AS created_by, A1.created_dt AS created_dt, (SELECT employee_name FROM employee WHERE id = A1.created_by) AS created_by, DATE_FORMAT(A1.created_dt, '%e %M %Y %H:%i:%s') AS created_dt, (SELECT employee_name FROM employee WHERE id = A1.update_by) AS update_by, DATE_FORMAT(A1.update_dt, '%e %M %Y %H:%i:%s') AS update_dt, A1.start_cuti, A1.end_cuti, A1.cuti_phone, (SELECT employee_name FROM employee WHERE id = A1.pengganti_cuti) AS pengganti_cuti
     FROM permission_log A1
     JOIN employee A2 ON A1.employee_id = A2.id
     JOIN department A3 ON A2.department_id = A3.department_id
